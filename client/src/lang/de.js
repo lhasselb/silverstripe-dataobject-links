d8944c9133f97e41beb5a35c2fdc573faf1cacea
lhasselb <guggelimehl@googlemail.com>
2019-02-22 10:36:23 +0100

    Add de

---
+++ client/src/lang/de.js
@@ -0,0 +1,12 @@
+
+if (typeof ss === 'undefined' || typeof ss.i18n === 'undefined') {
+    if (typeof console !== 'undefined') {
+      // eslint-disable-line no-console
+      console.error('Class ss.i18n not defined'); // eslint-disable-line no-console
+    }
+  } else {
+    ss.i18n.addDictionary('de', {
+      'CMS.LINKLABEL_PAGE': 'Link zu einem Objekt',
+      'CMS.LINK_PAGE': 'Link zu einem Objekt'
+    });
+  }
