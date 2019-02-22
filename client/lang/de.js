738d476bd54d95e83d504d1d534f05a46dca8f77
lhasselb <guggelimehl@googlemail.com>
2019-02-22 16:13:23 +0100

    Improve translation

--- client/lang/de.js
+++ client/lang/de.js
@@ -1,12 +1,11 @@
-
 if (typeof ss === 'undefined' || typeof ss.i18n === 'undefined') {
     if (typeof console !== 'undefined') {
       // eslint-disable-line no-console
       console.error('Class ss.i18n not defined'); // eslint-disable-line no-console
     }
   } else {
-    ss.i18n.addDictionary('de', {
-      'CMS.LINKLABEL_OBJECT': 'Link zu einem Objekt',
-      'CMS.LINK_OBJECT': 'Link zu einem Objekt'
+    ss.i18n.addDictionary('de_DE', {
+      "DATAOBJECT_LINKS.LINKLABEL_OBJECT": "Link zu einem Objekt",
+      "DATAOBJECT_LINKS.LINK_OBJECT": "Link zu einem Objekt"
     });
   }
